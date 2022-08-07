41a2d612b3e55f2e878e01a9d0625f0780bd3d3c
ShaheerRehman <shaheerr7@gmail.com>
2022-08-07 21:29:49 +0500

    ready

--- movierater/settings.py
+++ movierater/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 from dj_database_url import parse as dburl
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['movie-rater-shaheer.herokuapp.com']
 # ALLOWED_HOSTS = ['0.0.0.0',]
 
 # Application definition
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'movierater.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-default_dburl = 'sqlite:///' + str(Path.joinpath(BASE_DIR, 'db.sqlite3'))
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
